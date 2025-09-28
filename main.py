30540631ec1388ce991f56b5501b53fbcca547dc
Sergio Gonzalez <swgonzalez@unbosque.edu.co>
2025-09-28 10:53:09 -0500

    28 sep, funciona bien

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ def generate_invoice(Request: Request):
     }
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", reload=True)
