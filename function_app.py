b744e6ea8ba44b59d08a36e991b812d0e3b68253
Sergio Gonzalez <swgonzalez@unbosque.edu.co>
2025-10-08 20:46:01 -0500

    try

--- function_app.py
+++ function_app.py
@@ -7,14 +7,28 @@ app = func.FunctionApp()
 
 @app.route(route="GenerateInvoice")
 def GenerateInvoice(req: func.HttpRequest) -> func.HttpResponse:
-    Data = req.get_json()
-    RequestObj = Request(**Data)
-    GenerateXML(RequestObj,"Invoice")
-    return func.HttpResponse(
+    try:
+        Data = req.get_json()
+    except ValueError:
+        return func.HttpResponse(
+            "Invalid JSON body",
+            status_code=400
+        )
+
+    try:
+        RequestObj = Request(**Data)
+        GenerateXML(RequestObj, "Invoice")
+        return func.HttpResponse(
             str(Data),
             status_code=200
-    )
-
+        )
+    except Exception as e:
+        return func.HttpResponse(
+            f"Error interno: {e}",
+            status_code=500
+        )
+    
+    
 @app.route(route="GenerateCreditNote")
 def GenerateCreditNote(req: func.HttpRequest) -> func.HttpResponse:
     Data = req.get_json()
