e798324c53102c2b533641a7e675388c12225d7b
Sergio Gonzalez <swgonzalez@unbosque.edu.co>
2025-10-08 19:41:42 -0500

    All in azure function

--- function_app.py
+++ function_app.py
@@ -1,27 +1,26 @@
 import azure.functions as func
-import datetime
-import json
-import logging
+from models import Request
+from GenerateXML import GenerateXML
+
 
 app = func.FunctionApp()
 
-@app.route(route="HttpExample")
-def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+@app.route(route="GenerateInvoice")
+def GenerateInvoice(req: func.HttpRequest) -> func.HttpResponse:
+    Data = req.get_json()
+    RequestObj = Request(**Data)
+    GenerateXML(RequestObj,"Invoice")
+    return func.HttpResponse(
+            str(Data),
+            status_code=200
+    )
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+@app.route(route="GenerateCreditNote")
+def GenerateCreditNote(req: func.HttpRequest) -> func.HttpResponse:
+    Data = req.get_json()
+    RequestObj = Request(**Data)
+    GenerateXML(RequestObj,"CreditNote")
+    return func.HttpResponse(
+            str(Data),
+            status_code=200
+    )
